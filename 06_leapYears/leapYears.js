9f836992c87794868e4a225ae1d66e78498a5a81
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-04-29 18:53:58 -0500

    Finished Exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDivByFour = year % 4 === 0;
+    const isYearDivByHundred = year % 100 !== 0;
+    const isYearDivByFourHundred = year % 400 === 0;
 
+    if (isYearDivByFour && (isYearDivByHundred || isYearDivByFourHundred)){
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
