938562915519c8621d5314888723a032c5cd7428
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-04-28 21:09:37 -0500

    Finished remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers, ...valuesToRemove) {
+    //let RemoveLength = valuesToRemove.length;
+    let modArray = numbers;
+    for (let i = 0; i <= valuesToRemove.length; i++ ){
+        modArray = modArray.filter(number => number !== valuesToRemove[i]);
+    }
+    return modArray;
 };
 
 // Do not edit below this line
