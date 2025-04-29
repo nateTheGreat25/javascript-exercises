c666ddb9864fcce5f345e482b13c7a1f59777ef3
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-04-28 21:56:27 -0500

    Finshed sum all good enoug not a fan of them not telling me everthing I need to do in the readme

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let begin =0
+    let sumOfAllValues = 0;
+    for (let i = start; i <= end; i++){
+        sumOfAllValues += i;
+    }
+   
+    
+    return sumOfAllValues;
+    
 };
+sumAll(2,4);
 
 // Do not edit below this line
 module.exports = sumAll;
