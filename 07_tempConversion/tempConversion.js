c2b108dfa038170dc8841d61176f3859d911684a
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-04-29 19:07:07 -0500

    Finished temp convert

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  let cel = (faren - 32) / 1.8;
+  cel = Math.round(cel * 10) / 10;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let faren = cel * 1.8 + 32;
+  faren = Math.round(faren * 10) /10;
+  return faren;
 };
 
 // Do not edit below this line
