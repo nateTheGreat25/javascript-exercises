e0cc560e5a4225b5dd0bf4f4a8086a7cdb71c549
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-05-15 20:53:24 -0500

    finsihed repeat string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numRepeat) {
+    let stringRep = "";
+    for(let i = 0; i < numRepeat; i++){
+        stringRep += string;
+    }
+    if(numRepeat < 0){
+        return 'ERROR';
+    }else {
+    return stringRep;
+    }
 };
 
 // Do not edit below this line
