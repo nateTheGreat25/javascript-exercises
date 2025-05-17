d3c9bdd7a9ed72b9adfedaee5e56111b8d4a0428
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-05-16 23:52:18 -0500

    finished remove from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    let newArr;
+    toRemove.forEach((numb) => {
+        arr = arr.filter((itemArr) => itemArr !== numb)
+    });
+    return arr;
 };
 
 // Do not edit below this line
