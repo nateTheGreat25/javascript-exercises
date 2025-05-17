48b6d866f4a1cae768ed731d91f7d3faac1d8302
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-05-17 00:28:54 -0500

    Finished um all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if(start > end) {
+        [start, end] = [end, start]
+        
+    }
+    if(Number.isInteger(start) && Number.isInteger(end) && start > 0 && end > 0) {
+        for(let i = start; i <= end; i++) {
+            
+            sum += i;
+            
+        }
+    }else {
+        sum = 'ERROR';
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
