b119b2d1e1d8c9aaa85860637d5e4ad761e717de
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-05-19 20:58:50 -0500

    got palindrome done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const charsToRemove = ['!',' ', ',', '.'];
+    str = str.toLowerCase();
+    str = [...str];
 
+    let chars = str.filter(char => !charsToRemove.includes(char));
+
+     let reverseChars = chars.slice().reverse();
+     
+     chars = chars.join('')
+     reverseChars = reverseChars.join('')
+
+     //console.log(chars);
+     //console.log(reverseChars);
+
+     if(chars === reverseChars) {
+        return true;
+     }else {
+        return false;
+     }
+     
+    
 };
+//palindromes("r3ace3car");
 
 // Do not edit below this line
 module.exports = palindromes;
